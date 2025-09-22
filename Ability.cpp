11868df18cf8f38ac04dea023ddfd596d1fb18c0
shawlucas19 <a1886336@adelaide.edu.au>
2025-09-22 17:44:40 +0930

    testing some code for main classes

--- Ability.cpp
+++ Ability.cpp
@@ -8,10 +8,4 @@ Ability::Ability(std::string abilityName, Attribute type, double damage, double 
     this->hitChance = hitChance;
     this->description = description;
 }
-Ability::~Ability() = default;
-
-std::string& Ability::name() const { return n; }
-Attribute Ability::type() const { return type; }
-int Ability::power() const { return power_; }
-double Ability::hitChance() const { return hitChance_; }
-const std::string& Ability::description() const { return desc_; }
+Ability::~Ability(){}
