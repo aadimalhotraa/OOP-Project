6075f5650bce1fdb0f49ceaa071bef8d37b18d9d
shawlucas19 <a1886336@adelaide.edu.au>
2025-09-23 17:13:56 +0930

    stuff

--- Ability.cpp
+++ Ability.cpp
@@ -3,9 +3,14 @@
 
 Ability::Ability(std::string abilityName, Attribute type, double damage, double hitChance, std::string description){
     this->abilityName = abilityName;
-    this->type = type;
-    this->damage = damage;
-    this->hitChance = hitChance;
+    this->type        = type;
+    this->damage      = damage;
+    this->hitChance   = hitChance;
     this->description = description;
 }
+std::string& Ability::getName()             { return abilityName; }
+Attribute          Ability::getType()       { return type; }
+double             Ability::getDamage()     { return damage; }
+double             Ability::getHitChance()  { return hitChance; }
+std::string& Ability::getDescription()      { return description; }
 Ability::~Ability(){}
