5560bbc99fc6b35cf0f8c87fc8c78fca98b3c39a
shawlucas19 <a1886336@adelaide.edu.au>
2025-10-01 13:02:10 +0930

    small changes from last night

--- Player.h
+++ Player.h
@@ -1,15 +1,19 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "Character.h"
-#include "Ability.h"
+#include "Fire.h"
+#include "Water.h"
+#include "Grass.h"
+#include "Dark.h"
+#include "Light.h"
 #include <string>
 
 class Player: public Character{
     private:
         std::string playerName;
+        int kills;              // every 5 kills learn ability this variable is to keep track
     public:
         void levelUp();
-
 
 };
 #endif
